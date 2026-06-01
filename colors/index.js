be21590e05fad1e71d1592998509b9c6c6394f46
Musely <jboysamuel08@gmail.com>
2026-06-01 06:48:30 -0700

    still building

--- colors/index.js
+++ colors/index.js
@@ -10,5 +10,16 @@ class Color {
 }
 
 const allColors = [
-  new Color ('brightred', '#E74c')
-]
+  new Color ('brightred', '#E74c3c3'),
+  new Color ('soothinpurple', '#9b59b6'),
+  new Color ('skyblue', '#48c9b0'),
+  new Color ('leafygreen', '#5dad32'),
+  new Color ('sunkissedyellow', '#f4d03f'),
+  new Color ('groovegray', '#d7dbdd'),
+];
+
+exports.getRandomColor = () => {
+  return allColors[Math.floor(Math.random() * allColors.length);]
+}
+
+exports.allColors = allColors;
